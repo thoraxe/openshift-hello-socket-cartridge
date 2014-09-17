937f28e5f0a7436409f8415c322d0863f2dac090
Erik M Jacobs <erikmjacobs@gmail.com>
2014-09-17 15:22:26 -0400

    added file that gets read and regurgitated

--- usr/hellosocket.rb
+++ usr/hellosocket.rb
@@ -7,8 +7,10 @@ log = Logger.new("#{ENV["OPENSHIFT_LOG_DIR"]}/hskt.log")
 log.debug("Log file opened...")
 loop do
   client = server.accept    # Wait for a client to connect
-  client.puts "Hello socket!"
-  client.puts "Time is #{Time.now}"
+  client.puts "Hello socket! The time is #{Time.now}\r\n"
+  File.readlines("#{ENV["OPENSHIFT_REPO_DIR"]}/outputfile.txt").each do |line|
+    client.puts "#{line}"
+  end
   log.debug("Client connected: #{Time.now}")
   client.close
 end
