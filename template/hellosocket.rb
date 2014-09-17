06f4a66f75884eb5d649d54662e45f05e9b60bfe
Erik M Jacobs <erikmjacobs@gmail.com>
2014-09-17 10:27:40 -0400

    Switched to using ruby logger

--- template/hellosocket.rb
+++ template/hellosocket.rb
@@ -1,10 +1,14 @@
 require 'socket'
+require 'logger'
 
+# set up our TCP server listener
 server = TCPServer.new("#{ENV["OPENSHIFT_HSKT_IP"]}",22222)
+log = Logger.new("#{ENV["OPENSHIFT_LOG_DIR"]}/hskt.log")
+log.debug("Log file opened...")
 loop do
   client = server.accept    # Wait for a client to connect
   client.puts "Hello socket!"
   client.puts "Time is #{Time.now}"
-  puts "Someone connected @#{Time.now}"
+  log.debug("Client connected: #{Time.now}")
   client.close
 end
